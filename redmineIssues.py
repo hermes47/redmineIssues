3605c0b83a05769eecbda11e20d17f96a1d4bfe2
Jonny Paton <jonathan.r.paton@googlemail.com>
2015-11-30 17:04:16 +0000

    DID PYTHON
    ME DONE PYTHON

--- redmineIssues.py
+++ redmineIssues.py
@@ -1,34 +1,28 @@
-﻿from sopel import module
+from sopel import module
 from redmine import Redmine
-from redmine.exceptions import AuthError, ResourceNotFoundError
-from redmine.packages.requests.exceptions import MissingSchema
 import re
 
-rm = Redmine('');
+rm = Redmine("");
 
-@module.rule('.*#\d+')
+@module.rule(r".*#\d+")
 def bug_subject(bot, trigger):
-    matches = re.findall('#\d+',trigger.group(0))
+    matches = re.findall(r"#(\d+)",trigger.group(0))
     for match in matches:
-        bugNum = re.search('\d+',match)
         try:
-            bot.say(rm.issue.get(int(bugNum.group(0))).subject)
-            bot.say(rm.url + '/issues/' + bugNum.group(0))
-        except MissingSchema:
-            bot.reply('No URL Set')
-        except AuthError:
-            bot.reply('No Key Set')
-        except ResourceNotFoundError:
-            bot.reply('Issue not found')
+            bot.say(rm.issue.get(int(match)).subject)
+        except Exception as ex:
+            bot.say(ex)
+        else:
+        	bot.say(rm.url + "/issues/" + match)
 
-@module.commands('seturl')
+@module.commands(r"seturl")
 def set_url(bot, trigger):
-    if(trigger.owner):
+    if trigger.owner is True:
         rm.url = trigger.group(2)
-        bot.reply('Redmine URL set to ' + trigger.group(2))
+        bot.reply("Redmine URL set to " + trigger.group(2))
 
-@module.commands('setapikey')
+@module.commands(r"setapikey")
 def set_api_key(bot, trigger):
-    if(trigger.owner):
+    if trigger.owner is True:
         rm.key = trigger.group(2)
-        bot.reply('Redmine key set to ' + trigger.group(2))
+        bot.reply("Redmine key set to " + trigger.group(2))
