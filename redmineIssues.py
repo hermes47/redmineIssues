90b85ee723898245ada51ea6eb46f0f7805a11c8
Jonny Paton <jonathan.r.paton@googlemail.com>
2015-12-04 15:13:39 +0000

    Add config settings
    URL and API key are stored in config file now, so don't need to be set each time it's loaded

--- redmineIssues.py
+++ redmineIssues.py
@@ -1,8 +1,18 @@
-from sopel import module
+from sopel import module, config
+from sopel.config.types import StaticSection, ValidatedAttribute
 from redmine import Redmine
 import re
 
 rm = Redmine("");
+
+class redmineSection(StaticSection):
+    url = ValidatedAttribute('url')
+    apikey = ValidatedAttribute('apikey')
+
+def setup(bot):
+    bot.config.define_section("redmine", redmineSection)
+    rm.key = bot.config.redmine.apikey
+    rm.url = bot.config.redmine.url
 
 @module.rule(r".*#\d+")
 def bug_subject(bot, trigger):
@@ -19,10 +29,12 @@ def bug_subject(bot, trigger):
 def set_url(bot, trigger):
     if trigger.owner is True:
         rm.url = trigger.group(2)
+        bot.config.redmine.url = rm.url
         bot.reply("Redmine URL set to " + trigger.group(2))
 
 @module.commands(r"setapikey")
 def set_api_key(bot, trigger):
     if trigger.owner is True:
         rm.key = trigger.group(2)
+        bot.config.redmine.apikey = rm.key;
         bot.reply("Redmine key set to " + trigger.group(2))
