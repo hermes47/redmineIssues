fee775f33943f23f3a8db13b53069b28e6865b90
Jonny Paton <jonathan.r.paton@googlemail.com>
2015-12-05 16:30:53 +0000

    Improved channel restriction
    Owner has to manually set url and API key for specific channels. These are stored in a database

--- redmineIssues.py
+++ redmineIssues.py
@@ -1,52 +1,38 @@
-from sopel import module, config, trigger
+from sopel import module, config, trigger, db
 from sopel.config.types import StaticSection, ValidatedAttribute
+from sopel.db import SopelDB
 from redmine import Redmine
 import re
 
-class redmineSection(StaticSection):
-    url = ValidatedAttribute('url')
-    apikey = ValidatedAttribute('apikey')
-    channel = ValidatedAttribute('channel')
-
 class redmineIssues():
-    channel = None
+    rmdb = SopelDB
+    rm = Redmine("")
 
 def setup(bot):
-    bot.config.define_section("redmine", redmineSection)
-    rm.key = bot.config.redmine.apikey
-    rm.url = bot.config.redmine.url
-    redmineIssues.channel = bot.config.redmine.channel
-    
+    redmineIssues.rmdb = SopelDB(bot.config)
+
 @module.rule(r".*#\d+")
 def bug_subject(bot, trigger):
-    if redmineIssues.channel is None or redmineIssues.channel == trigger.sender:
-        matches = re.findall(r"#(\d+)",trigger.group(0))
-        for match in matches:
-            try:
-                bot.say(rm.issue.get(int(match)).subject)
-            except Exception as ex:
-                bot.say(ex)
-            else:
-                bot.say(rm.url + "/issues/" + match)
+    redmineIssues.rm.key = redmineIssues.rmdb.get_channel_value(trigger.sender,"apikey")
+    redmineIssues.rm.url = redmineIssues.rmdb.get_channel_value(trigger.sender,"url")
+    matches = re.findall(r"#(\d+)",trigger.group(0))
+    for match in matches:
+        try:
+            issue = redmineIssues.rm.issue.get(int(match))
+        except Exception as ex:
+            bot.say(ex)
+        else:
+            bot.say(issue.subject)
+            bot.say(rm.url + "/issues/" + match)
 
 @module.commands(r"seturl")
 def set_url(bot, trigger):
     if trigger.owner is True:
-        rm.url = trigger.group(2)
-        bot.config.redmine.url = rm.url
-        bot.reply("Redmine URL set to " + trigger.group(2))
+        redmineIssues.rmdb.set_channel_value(trigger.group(3),"url",trigger.group(4))
+        bot.reply("Redmine URL for channel " + trigger.group(3) + " set to " + trigger.group(4))
 
 @module.commands(r"setapikey")
 def set_api_key(bot, trigger):
     if trigger.owner is True:
-        rm.key = trigger.group(2)
-        bot.config.redmine.apikey = rm.key;
-        bot.reply("Redmine key set to " + trigger.group(2))
-
-@module.commands(r"lockchannel")
-def lock_channel(bot, trigger):
-    if trigger.owner is True:
-        redmineIssues.channel = trigger.group(2)
-        bot.config.redmine.channel = redmineIssues.channel
-        bot.reply("Locking to channel " + trigger.group(2))
-       
+        redmineIssues.rmdb.set_channel_value(trigger.group(3),"apikey",trigger.group(4))
+        bot.reply("Redmine key for channel " + trigger.group(3) + " set to " + trigger.group(4))
