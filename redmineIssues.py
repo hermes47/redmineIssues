d707f7d773a531b1b557f917f7582b801a04abb0
Jonny Paton <jonathan.r.paton@googlemail.com>
2015-12-05 18:10:40 +0000

    Tidied up a little

--- redmineIssues.py
+++ redmineIssues.py
@@ -1,38 +1,48 @@
 from sopel import module, config, trigger, db
-from sopel.config.types import StaticSection, ValidatedAttribute
 from sopel.db import SopelDB
 from redmine import Redmine
+from redmine.resources import Version, Project, Issue
 import re
 
-class redmineIssues():
-    rmdb = SopelDB
-    rm = Redmine("")
-
 def setup(bot):
-    redmineIssues.rmdb = SopelDB(bot.config)
+    bot.rmdb = SopelDB(bot.config)
+    bot.rm = Redmine("")
 
 @module.rule(r".*#\d+")
 def bug_subject(bot, trigger):
-    redmineIssues.rm.key = redmineIssues.rmdb.get_channel_value(trigger.sender,"apikey")
-    redmineIssues.rm.url = redmineIssues.rmdb.get_channel_value(trigger.sender,"url")
+    bot.rm.key = bot.rmdb.get_channel_value(trigger.sender,"apikey")
+    bot.rm.url = bot.rmdb.get_channel_value(trigger.sender,"url")
     matches = re.findall(r"#(\d+)",trigger.group(0))
     for match in matches:
         try:
-            issue = redmineIssues.rm.issue.get(int(match))
+            issue = bot.rm.issue.get(int(match))
         except Exception as ex:
             bot.say(ex)
         else:
             bot.say(issue.subject)
-            bot.say(rm.url + "/issues/" + match)
+            bot.say(bot.rm.url + "/issues/" + match)
 
 @module.commands(r"seturl")
 def set_url(bot, trigger):
     if trigger.owner is True:
-        redmineIssues.rmdb.set_channel_value(trigger.group(3),"url",trigger.group(4))
+        bot.rmdb.set_channel_value(trigger.group(3),"url",trigger.group(4))
         bot.reply("Redmine URL for channel " + trigger.group(3) + " set to " + trigger.group(4))
 
 @module.commands(r"setapikey")
 def set_api_key(bot, trigger):
     if trigger.owner is True:
-        redmineIssues.rmdb.set_channel_value(trigger.group(3),"apikey",trigger.group(4))
+        bot.rmdb.set_channel_value(trigger.group(3),"apikey",trigger.group(4))
         bot.reply("Redmine key for channel " + trigger.group(3) + " set to " + trigger.group(4))
+
+@module.commands(r"setProject")
+def set_project(bot,trigger):
+    if trigger.owner is True:
+        bot.rmdb.set_channel_value(trigger.group(3),"project",trigger.group(4))
+        bot.reply("Redmine key for channel " + trigger.group(3) + " set to " + trigger.group(4))
+
+@module.commands(r"build")
+def get_build(bot, trigger):
+    bot.rm.key = bot.rmdb.get_channel_value(trigger.sender,"apikey")
+    bot.rm.url = bot.rmdb.get_channel_value(trigger.sender,"url")
+    project = bot.rm.project.get(bot.rmdb.get_channel_value(trigger.sender,"project"))
+    bot.reply(project.versions[len(project.versions)-3])
